25489737e8e481cb5763a40fe6438f37a6715313
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-02-22 04:07:47 +0000

    Site files

--- docs/docs.sh
+++ docs/docs.sh
@@ -1,1 +1,21 @@
-../docs.sh
+#!/usr/bin/bash
+
+function github_pages()
+{
+  module load python/3.7
+  source ~/COVID-19/py37/bin/activate
+# pip install mkdocs-mermaid2-plugin
+}
+
+github_pages
+mkdocs build
+mkdocs gh-deploy
+
+git add .gitignore
+git add docs
+git add README.md
+git add mkdocs.yml
+git commit -m "Site files"
+git add 1_desc.sh
+git commit -m "script"
+git push
