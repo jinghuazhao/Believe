598f4396244754d2bc1c7105432d780826c9ef4f
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-06-29 16:49:58 +0100

    script

--- 0_uitls.sh
+++ 0_uitls.sh
@@ -10,45 +10,12 @@ export somalogic=${pre_qc}/somalogic_proteomics
 Rscript -e '
 library(arrow)
 olink <- Sys.getenv("olink")
-npx <- read_parquet("Q-08620_Di_Angelantonio_NPX_2023-12-27.parque",
-                    n_threads = 4,
-                    compression = "gzip")
-library(OlinkAnalyze)
-npx_data <- read_NPX(filename = file.path(olink,
-                                "Q-08620_DiAngelantonio_-_Olink_-_Sample_manifest_-_plate_Explore_HT_20231120.xlsx"))
-# visualize the NPX distribution per sample per panel, example for one panel
-olink_dist_plot(npx_data %>%
-  filter(Panel == 'Olink CARDIOMETABOLIC')) +
-  theme(axis.text.x = element_blank(),
-        axis.ticks.x = element_blank()) +
-  scale_fill_manual(values = c('turquoise3', 'red'))
-olink_qc_plot(npx_data1 %>% filter(Panel == 'Olink CARDIOMETABOLIC')) +
-  scale_color_manual(values = c('turquoise3', 'red'))
-# identify bridge samples
-bridge_samples <- intersect(x = npx_data1$SampleID,
-               y = npx_data2$SampleID)
-# bridge normalize
-bridge_normalized_data <- olink_normalization(df1 = npx_data1,
-                          df2 = npx_data2,
-                          overlapping_samples_df1 = bridge_samples,
-                          df1_project_nr = "20200001",
-                          df2_project_nr = "20200002",
-                          reference_project = "20200001")
-# t-test npx_data
-ttest_results_NPX1 <- olink_ttest(df = npx_data, variable = "Treatment")
-# select names of the top #10 most significant proteins
-ttest_sign_NPX1 <- ttest_results_NPX1 %>%
-    head(n=10) %>%
-    pull(OlinkID)
-# volcano plot with annotated top #10 most significant proteins
-olink_volcano_plot(p.val_tbl = ttest_results_NPX1,
-  olinkid_list = ttest_sign_NPX1) +
-  scale_color_manual(values = c('turquoise3', 'red'))
+npx <- read_parquet(file.path(olink,"Q-08620_Di_Angelantonio_NPX_2023-12-27.parquet"))
 # Q-08620_Di_Angelantonio_Analysis_Report_2023-12-27.pdf
 # Q-08620_DiAngelantonio_-_Olink_-_Explore_HT_SampleForm_v2.0.pdf
 # Q-08620_DiAngelantonio_-_Olink_-_Sample_manifest_-_plate_Explore_HT_20231120.xlsx
 
-library(SomaDataIO)
+suppressMessages(library(SomaDataIO))
 somalogic <- Sys.getenv("somalogic")
 # batch 1, SS-2218747_SQS.pdf
 adat_samples <- read.delim(file.path(somalogic,"Batch1","Batch1_adat_samplenames.tsv"))
@@ -77,4 +44,44 @@ anmlSMP <- SomaDataIO::read_adat(file.path(somalogic,"Batch1_Batch2_combined",
 # SSM-00060 - Rev 1.0 - Data Standardization and File Specification Technical Note (002).pdf'
 # Test Information Guides Plasma PAV/
 # Test Information Guides Plasma SST/
+
+if (FALSE)
+{
+  Sys.setenv(LIBARROW_MINIMAL = "false") # all optional features including gzip
+  Sys.setenv(ARROW_WITH_GZIP = "ON") # only gzip
+  install.packages("arrow")
+  install_arrow()
+  library(OlinkAnalyze)
+  npx_data <- read_NPX(filename = file.path(olink,
+                                  "Q-08620_DiAngelantonio_-_Olink_-_Sample_manifest_-_plate_Explore_HT_20231120.xlsx"))
+  # visualize the NPX distribution per sample per panel, example for one panel
+  olink_dist_plot(npx_data %>%
+    filter(Panel == 'Olink CARDIOMETABOLIC')) +
+    theme(axis.text.x = element_blank(),
+          axis.ticks.x = element_blank()) +
+    scale_fill_manual(values = c('turquoise3', 'red'))
+  olink_qc_plot(npx_data1 %>% filter(Panel == 'Olink CARDIOMETABOLIC')) +
+    scale_color_manual(values = c('turquoise3', 'red'))
+  # identify bridge samples
+  bridge_samples <- intersect(x = npx_data1$SampleID,
+                 y = npx_data2$SampleID)
+  # bridge normalize
+  bridge_normalized_data <- olink_normalization(df1 = npx_data1,
+                            df2 = npx_data2,
+                            overlapping_samples_df1 = bridge_samples,
+                            df1_project_nr = "20200001",
+                            df2_project_nr = "20200002",
+                            reference_project = "20200001")
+  # t-test npx_data
+  ttest_results_NPX1 <- olink_ttest(df = npx_data, variable = "Treatment")
+  # select names of the top #10 most significant proteins
+  ttest_sign_NPX1 <- ttest_results_NPX1 %>%
+      head(n=10) %>%
+      pull(OlinkID)
+  # volcano plot with annotated top #10 most significant proteins
+  olink_volcano_plot(p.val_tbl = ttest_results_NPX1,
+    olinkid_list = ttest_sign_NPX1) +
+    scale_color_manual(values = c('turquoise3', 'red'))
+}
 '
+
