a6d54a1ed01d7157a2c8de5ba2cf3d26e568b71b
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-07-08 12:57:23 +0100

    script

--- 0_uitls.sh
+++ 0_uitls.sh
@@ -8,6 +8,8 @@ export olink=${pre_qc}/olink_proteomics
 export somalogic=${pre_qc}/somalogic_proteomics
 
 Rscript -e '
+options(width=200)
+supppressMessages)library(dplyr))
 # Olink
 Sys.setenv(LIBARROW_MINIMAL = "false") # all optional features including gzip
 Sys.setenv(ARROW_WITH_GZIP = "ON") # only gzip
@@ -16,7 +18,8 @@ library(OlinkAnalyze)
 olink <- Sys.getenv("olink")
 npx <- read_parquet(file.path(olink,"Q-08620_Di_Angelantonio_NPX_2023-12-27.parquet"))
 table(npx$SampleType)
-npx[c("SampleID","OlinkID","Panel","PCNormalizedNPX","Count","NPX","UniProt","AssayQC","SampleQC")]
+npx[c("SampleID","OlinkID","Panel","PCNormalizedNPX","Count","NPX","UniProt","AssayQC","SampleQC")] %>%
+filter(NPX!=0)
 # Q-08620_Di_Angelantonio_Analysis_Report_2023-12-27.pdf
 # Q-08620_DiAngelantonio_-_Olink_-_Explore_HT_SampleForm_v2.0.pdf
 # Q-08620_DiAngelantonio_-_Olink_-_Sample_manifest_-_plate_Explore_HT_20231120.xlsx
@@ -71,8 +74,6 @@ adat <- SomaDataIO::read_adat(file.path(somalogic,"Batch1_Batch2_combined",
 anmlSMP <- SomaDataIO::read_adat(file.path(somalogic,"Batch1_Batch2_combined",
            "SS-2218747_SS-2225082_Combined_v4.1_EDTAPlasma.hybNorm.medNormInt.plateScale.calibration.anmlQC.qcCheck.anmlSMP.adat"))
 
-options(width=200)
-library(dplyr)
 library(Biobase)
 class(adat)
 dim(adat)
@@ -86,6 +87,7 @@ getAnalyteInfo(cleanData) |>
   select(AptName, SeqId, Target = TargetFullName, EntrezGeneSymbol, UniProt)
 
 adat_eset <- SomaDataIO::adat2eSet(cleanData)
+protData <- exprs(adat_eset)
 clinData <- pData(adat_eset)
 
 # SSM-00060 - Rev 1.0 - Data Standardization and File Specification Technical Note (002).pdf'
@@ -94,17 +96,37 @@ clinData <- pData(adat_eset)
 
 position <- function()
 {
+  options(width=200)
   suppressMessages(library(SomaScan.db))
+  suppressMessages(library(EnsDb.Hsapiens.v75))
+  library(org.Hs.eg.db)
+  library(AnnotationDbi)
+  library(TxDb.Hsapiens.UCSC.hg38.knownGene)
   keys(SomaScan.db)
   keytypes(SomaScan.db)
   columns(SomaScan.db)
-  select(SomaScan.db, keys = "18342-2", columns = c("SYMBOL", "UNIPROT"))
-  pos_sel <- select(SomaScan.db, "11138-16", columns = c("SYMBOL", "ENTREZID", "ENSEMBL"))
+  select(SomaScan.db, keys = "18342-2", columns = c("ENTREZID","SYMBOL","UNIPROT"))
   keys(EnsDb.Hsapiens.v75)[1:10L]
   grep("ENSEMBL", columns(SomaScan.db), value = TRUE)
   columns(EnsDb.Hsapiens.v75)
+  pos_sel <- select(SomaScan.db, "11138-16", columns = c("PMID","ENTREZID","SYMBOL","ENSEMBL"))
   pos_res <- select(EnsDb.Hsapiens.v75, keys = "ENSG00000020633",
-                    columns = c("GENEBIOTYPE", "SEQCOORDSYSTEM", "GENEID",
-                                "PROTEINID", "PROTDOMSTART", "PROTDOMEND"))
+                    columns = c("GENEID","TXNAME","SEQNAME","TXSEQSTART","TXSEQEND"))
   merge(pos_sel, pos_res, by.x = "ENSEMBL", by.y = "GENEID")
+# loop-over
+  results <- data.frame()
+  for (key in keys(SomaScan.db)) {
+    pos_sel <- select(SomaScan.db, keys = key, columns = c("PMID", "ENTREZID", "SYMBOL", "ENSEMBL"))
+    if (!is.null(pos_sel$ENSEMBL) && any(!is.na(pos_sel$ENSEMBL))) {
+      ensembl_ids <- pos_sel$ENSEMBL[!is.na(pos_sel$ENSEMBL)]
+      for (ensembl_id in unique(ensembl_ids)) {
+        pos_res <- select(EnsDb.Hsapiens.v75, keys = ensembl_id,
+                          columns = c("GENEID", "TXNAME", "SEQNAME", "TXSEQSTART", "TXSEQEND"))
+        merged_res <- merge(pos_sel[pos_sel$ENSEMBL == ensembl_id, ], pos_res,
+                            by.x = "ENSEMBL", by.y = "GENEID", all = TRUE)
+        results <- rbind(results, merged_res)
+      }
+    }
+  }
+  save(results,file="work/SomaScan.rda")
 }
